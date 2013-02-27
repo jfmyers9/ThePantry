422a656b23e4ae0b9f6c760c378e56079c7a067d
James Myers <jfmyers9@gmail.com>
2013-02-26 18:38:47 -0800

    first commit

---
+++ ThePantry/gen/cs169/project/thepantry/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cs169.project.thepantry;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ thePantryBackend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThePantryBackend::Application.config.secret_token = '04f35f7c5a16e9f0bcbb108d79ac919f2141207b7b31989314027ac5ab4b979b2c7363a5d3a9476e692018f2cf0da3cea03a181e07071121a7927eb9467e4d67'
