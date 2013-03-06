ea12a36b7a65176050866a915cc21a9aa6d99016
James Myers <jfmyers9@gmail.com>
2013-03-05 18:40:55 -0800

    this shit is annoying

--- ThePantry/gen/cs169/project/thepantry/R.java
+++ ThePantry/gen/cs169/project/thepantry/R.java
@@ -8,6 +8,12 @@
 package cs169.project.thepantry;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f070000;
+        public static final int pref_example_list_values=0x7f070001;
+        public static final int pref_sync_frequency_titles=0x7f070002;
+        public static final int pref_sync_frequency_values=0x7f070003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,29 +23,89 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_forgot_password=0x7f0a0008;
+        public static final int action_settings=0x7f0a0007;
+        public static final int email=0x7f0a0003;
+        public static final int login=0x7f0a0005;
+        public static final int login_form=0x7f0a0002;
+        public static final int login_status=0x7f0a0000;
+        public static final int login_status_message=0x7f0a0001;
+        public static final int password=0x7f0a0004;
+        public static final int sign_in_button=0x7f0a0006;
     }
     public static final class layout {
-        public static final int activity_inventory=0x7f030000;
-        public static final int activity_tutorial=0x7f030001;
+        public static final int activity_home_page=0x7f030000;
+        public static final int activity_inventory=0x7f030001;
+        public static final int activity_inventory_add=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_recipe=0x7f030004;
+        public static final int activity_search_results=0x7f030005;
+        public static final int activity_shopping_list=0x7f030006;
+        public static final int activity_tutorial=0x7f030007;
     }
     public static final class menu {
-        public static final int inventory=0x7f070000;
-        public static final int tutorial=0x7f070001;
+        public static final int home_page=0x7f090000;
+        public static final int inventory=0x7f090001;
+        public static final int inventory_add=0x7f090002;
+        public static final int login=0x7f090003;
+        public static final int recipe=0x7f090004;
+        public static final int search_results=0x7f090005;
+        public static final int shopping_list=0x7f090006;
+        public static final int tutorial=0x7f090007;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050003;
-        public static final int title_activity_inventory=0x7f050004;
-        public static final int title_activity_tutorial=0x7f050001;
+        public static final int action_forgot_password=0x7f06000f;
+        public static final int action_settings=0x7f060002;
+        public static final int action_sign_in_register=0x7f06000d;
+        public static final int action_sign_in_short=0x7f06000e;
+        public static final int app_name=0x7f060000;
+        public static final int error_field_required=0x7f060014;
+        public static final int error_incorrect_password=0x7f060013;
+        public static final int error_invalid_email=0x7f060011;
+        public static final int error_invalid_password=0x7f060012;
+        public static final int hello_world=0x7f060003;
+        public static final int login_progress_signing_in=0x7f060010;
+        public static final int pref_default_display_name=0x7f06001a;
+        public static final int pref_description_social_recommendations=0x7f060018;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f06001c;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f060016;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f06001f;
+        public static final int pref_ringtone_silent=0x7f060022;
+        public static final int pref_title_add_friends_to_messages=0x7f06001b;
+        public static final int pref_title_display_name=0x7f060019;
+        public static final int pref_title_new_message_notifications=0x7f060020;
+        public static final int pref_title_ringtone=0x7f060021;
+        public static final int pref_title_social_recommendations=0x7f060017;
+        public static final int pref_title_sync_frequency=0x7f06001d;
+        public static final int pref_title_system_sync_settings=0x7f06001e;
+        public static final int pref_title_vibrate=0x7f060023;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06000b;
+        public static final int prompt_password=0x7f06000c;
+        public static final int title_activity_home_page=0x7f060005;
+        public static final int title_activity_inventory=0x7f060004;
+        public static final int title_activity_inventory_add=0x7f060008;
+        public static final int title_activity_login=0x7f06000a;
+        public static final int title_activity_recipe=0x7f060009;
+        public static final int title_activity_search_results=0x7f060007;
+        public static final int title_activity_settings=0x7f060015;
+        public static final int title_activity_shopping_list=0x7f060006;
+        public static final int title_activity_tutorial=0x7f060001;
     }
     public static final class style {
         /** 
@@ -63,10 +129,17 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int LoginFormContainer=0x7f080002;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
---
+++ ThePantry/src/cs169/project/thepantry/SearchModel.java
@@ -0,0 +1,5 @@
+package cs169.project.thepantry;
+
+public class SearchModel {
+
+}
