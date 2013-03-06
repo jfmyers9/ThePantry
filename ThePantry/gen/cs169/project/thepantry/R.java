21a510342edfb2650d52b3f290113c5dfebbe6fd
mattvisco <mattvisco@mac.com>
2013-03-05 17:42:14 -0800

    inventory activity

--- ThePantry/gen/cs169/project/thepantry/R.java
+++ ThePantry/gen/cs169/project/thepantry/R.java
@@ -27,15 +27,18 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_tutorial=0x7f030000;
+        public static final int activity_inventory=0x7f030000;
+        public static final int activity_tutorial=0x7f030001;
     }
     public static final class menu {
-        public static final int tutorial=0x7f070000;
+        public static final int inventory=0x7f070000;
+        public static final int tutorial=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
+        public static final int title_activity_inventory=0x7f050004;
         public static final int title_activity_tutorial=0x7f050001;
     }
     public static final class style {
