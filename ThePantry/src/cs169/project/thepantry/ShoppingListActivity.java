7a0175d7aa016b2cae0c5a37ca92ca84f1d79820
Amy Zhang <amyzhang92@berkeley.edu>
2013-03-07 01:07:09 -0800

    Create basic UI Layout for Shopping List
    author: Amy Zhang
    
    This ShoppingListActivity layout consists of a field to add an
    ingredient, an add button, and a spinner to select ingredient types from
    a predetermined list (which can be easily edited).
    
    In addition to these changes, the shopping_list.xml menu page now has
    additional choices in the action bar drop down menu. Currently, these
    choices don't lead anywhere but ideally they will be used for
    navigation. Furthermore, we would not have login/logout/sign up all on
    the same page - it will be dependent on the state of the user, but for
    exploration purposes, they all exist together.
    
    Lastly, a variety of new strings now live in strings.xml. These are
    mostly the ones designed for the action bar (they all have the form
    "action_[name of activity]". There is also a string array for the
    ingredient category spinner that can be reused for the addInventory
    page.
    
    Hopefully this doesn't break anything!

--- ThePantry/src/cs169/project/thepantry/ShoppingListActivity.java
+++ ThePantry/src/cs169/project/thepantry/ShoppingListActivity.java
@@ -1,8 +1,14 @@
 package cs169.project.thepantry;
 
 import android.os.Bundle;
+
 import android.app.Activity;
+//import android.app.ActionBar;
+
 import android.view.Menu;
+
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class ShoppingListActivity extends Activity {
 
@@ -10,6 +16,17 @@ public class ShoppingListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_shopping_list);
+		// Only use action bar if we want to specify certain items on it
+		//ActionBar actionBar = getActionBar();
+		
+		// Creates and populates the ingredient type drop-down menu
+		Spinner spinner = (Spinner) findViewById(R.id.ingredient_types);
+		ArrayAdapter<CharSequence> adapter = 
+				ArrayAdapter.createFromResource(this,
+												R.array.ingredient_type_array,
+												android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
 	}
 
 	@Override
@@ -17,6 +34,26 @@ public class ShoppingListActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.shopping_list, menu);
 		return true;
+	}
+	
+	/** Adds the given item to the shopping list */
+	public void addItem() {
+		// calls DatabaseModel
+	}
+	
+	/** Removes the given item from the shopping list */
+	public void removeItem() {
+		// calls DatabaseModel
+	}
+	
+	/** Updates the inventory with items checked on the shopping list */
+	public void updateInventory() {
+		// calls DatabaseModel
+	}
+	
+	/** Marks a shopping list item as checked */
+	public void checkItem() {
+		// calls DatabaseModel
 	}
 
 }
