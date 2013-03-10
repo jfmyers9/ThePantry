da7e6079f186866d79808a21d2313bb7a6e1019c
James Myers <jfmyers9@gmail.com>
2013-03-09 20:29:05 -0800

    tutorial activity working

--- ThePantry/src/cs169/project/thepantry/TutorialActivity.java
+++ ThePantry/src/cs169/project/thepantry/TutorialActivity.java
@@ -1,15 +1,32 @@
 package cs169.project.thepantry;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.View;
 
 public class TutorialActivity extends Activity {
 
+	private static final String OPENED = "opened";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tutorial);
+		
+		SharedPreferences opened = PreferenceManager.getDefaultSharedPreferences(this);
+		if (opened.getBoolean(OPENED, false) == false) {
+			SharedPreferences.Editor editor = opened.edit();
+			editor.putBoolean(OPENED, true);
+			editor.commit();
+			setContentView(R.layout.activity_tutorial);
+		} else {
+			Context context = getApplicationContext();
+			Intent intent = new Intent(context, HomePageActivity.class);
+			startActivity(intent);
+		}
 	}
 
 	@Override
@@ -17,6 +34,18 @@ public class TutorialActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tutorial, menu);
 		return true;
+	}
+	
+	public void setupInventory(View view) {
+		Context context = getApplicationContext();
+		Intent intent = new Intent(context, InventoryAddActivity.class);
+		startActivity(intent);
+	}
+	
+	public void skipTutorial(View view) {
+		Context context = getApplicationContext();
+		Intent intent = new Intent(context, HomePageActivity.class);
+		startActivity(intent);
 	}
 
 }
