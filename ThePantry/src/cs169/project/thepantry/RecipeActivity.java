aa08f3dfb4a0348e1ccbc547badd757affc133fd
Christina Hang <christinah0113@gmail.com>
2013-03-09 21:12:03 -0800

    modified recipe activity

--- ThePantry/src/cs169/project/thepantry/RecipeActivity.java
+++ ThePantry/src/cs169/project/thepantry/RecipeActivity.java
@@ -3,6 +3,7 @@ package cs169.project.thepantry;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class RecipeActivity extends Activity {
 
@@ -17,6 +18,18 @@ public class RecipeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.recipe, menu);
 		return true;
+	}
+	
+	public void addToFavorites(View v) {
+		//TODO
+	}
+	
+	public void markCooked(View v) {
+		// TODO
+	}
+	
+	public void addToShopping(View v) {
+		//TODO
 	}
 
 }
