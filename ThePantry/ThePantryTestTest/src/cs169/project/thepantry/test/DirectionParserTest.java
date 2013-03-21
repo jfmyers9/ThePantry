bf435de9a4aaeb5c567f3927f8a34abbc5fa39e9
Justin Kay <justinkay92@gmail.com>
2013-03-20 21:06:40 -0700

    mergggg

--- ThePantry/ThePantryTestTest/src/cs169/project/thepantry/test/DirectionParserTest.java
+++ ThePantry/ThePantryTestTest/src/cs169/project/thepantry/test/DirectionParserTest.java
@@ -1,6 +1,7 @@
 package cs169.project.thepantry.test;
 
 import cs169.project.thepantry.DirectionParser;
+import cs169.project.thepantry.IngredientParser;
 import junit.framework.TestCase;
 
 public class DirectionParserTest extends TestCase {
@@ -18,4 +19,8 @@ public class DirectionParserTest extends TestCase {
 		dp.getDirections("http://breakfast.food.com/recipe/asparagus-omelette-wraps-232083");
 	}
 
+	public void testParseIngredients() {
+		String ingredientLine = "1 1/2 pounds (3 cups) unsalted beets, with bacon strips";
+		System.out.println(IngredientParser.parse(ingredientLine)[3]);
+	}
 }
