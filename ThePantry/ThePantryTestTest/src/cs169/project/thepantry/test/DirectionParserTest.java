66c60cbfc1b8fffe71dd0f74827b300edf698595
Justin Kay <justinkay92@gmail.com>
2013-03-31 15:48:56 -0700

    improved ingredient parser to handle weird things like rounded teaspoons and lines with no measurement like 4 pumpkins

--- ThePantry/ThePantryTestTest/src/cs169/project/thepantry/test/DirectionParserTest.java
+++ ThePantry/ThePantryTestTest/src/cs169/project/thepantry/test/DirectionParserTest.java
@@ -20,14 +20,15 @@ public class DirectionParserTest extends TestCase {
 	}
 
 	public void testParseIngredients() {
-		String ingredientLine = "1 1/2 pounds unsalted beets, with bacon strips";
+		//String ingredientLine = "1 1/2 pounds unsalted beets, with bacon strips";
 		//String ingredientLine = "2 rounded teaspoons prepared dijon style mustard";
+		String ingredientLine = "3 pretty good sized pumpkins";
 		String[] result = IngredientParser.parse(ingredientLine);
 		System.out.println("Results:");
-			System.out.println(result[0]);
-			System.out.println(result[1]);
-			System.out.println(result[2]);
-			System.out.println(result[3]);
+			System.out.println("Number: " + result[0]);
+			System.out.println("Measurement: " + result[1]);
+			System.out.println("Additional: " + result[2]);
+			System.out.println("Ingredients: " + result[3]);
 	}
 	
 }
