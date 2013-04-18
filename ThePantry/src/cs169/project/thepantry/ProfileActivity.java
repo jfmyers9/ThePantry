03b270fc2ca3cabb36200adec6ea940ae588e468
Christina Hang <christinah0113@gmail.com>
2013-04-18 00:42:30 -0700

    updated recipe and profile activity can now check cooked and favorited

--- ThePantry/src/cs169/project/thepantry/ProfileActivity.java
+++ ThePantry/src/cs169/project/thepantry/ProfileActivity.java
@@ -29,7 +29,6 @@ public class ProfileActivity extends BasicMenuActivity {
 	TextView allergic;
 	
 	// get tables to be filled in
-	ListView favorites;
 	ListView history;
 	
 	// database access
@@ -37,10 +36,8 @@ public class ProfileActivity extends BasicMenuActivity {
 	private static final String DATABASE_NAME = "thepantry";
 	private final String LOGGED_IN = "log_in";
 	
-	// set up listview for favorites and history
-	ArrayList<SearchMatch> faves = new ArrayList<SearchMatch>();
+	// set up listview for history
 	ArrayList<SearchMatch> cooked = new ArrayList<SearchMatch>();
-	SearchResultAdapter favesAdapter;
 	SearchResultAdapter cookAdapter;
 	
 	SearchModel sm = new SearchModel();
@@ -65,8 +62,6 @@ public class ProfileActivity extends BasicMenuActivity {
 			invSync.execute(ThePantryContract.Inventory.TABLE_NAME, login_status);
 			
 			// set text for textviews
-			TextView favorite_text = (TextView)findViewById(R.id.user_faves_text);
-			favorite_text.setText("Favorites");
 			TextView cooked_text = (TextView)findViewById(R.id.user_cooked_text);
 			cooked_text.setText("Cooking History");
 			
@@ -80,36 +75,16 @@ public class ProfileActivity extends BasicMenuActivity {
 			likes.setText("Likes: " + shared_pref.getString("likes", ""));
 			allergic.setText("Allergies: " + shared_pref.getString("allergies", ""));
 			
-			favorites = (ListView)findViewById(R.id.user_favorites);
-			favesAdapter = new SearchResultAdapter(this, faves);
-			favorites.setAdapter(favesAdapter);
-			
-			setFavorites();
-			
 			history = (ListView)findViewById(R.id.user_cook_history);
 			cookAdapter = new SearchResultAdapter(this, cooked);
 			history.setAdapter(cookAdapter);
 			
+			// fill in history list
 			setHistory();
-			
-			// fill in lists
-			favesAdapter.notifyDataSetChanged();
 			cookAdapter.notifyDataSetChanged();
 			
 			// when recipes are clicked
-			favorites.setOnItemClickListener(new OnItemClickListener() {
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-				    // When clicked
-					if (isOnline()){
-			    		SearchCriteria searchcriteria = new SearchCriteria("recipe", (String)view.getTag());
-			    		HomePageActivity hpActivity = new HomePageActivity();
-			    		HomeSearchTask hstask = hpActivity.new HomeSearchTask(getApplicationContext(), "recipe");
-			    		hstask.execute(searchcriteria);
-					}
-				}
-			});
-			
+
 			history.setOnItemClickListener(new OnItemClickListener() {
 				public void onItemClick(AdapterView<?> parent, View view,
 						int position, long id) {
@@ -122,6 +97,7 @@ public class ProfileActivity extends BasicMenuActivity {
 					}
 				}
 			});
+			
 		} else {
 			Intent i = new Intent(this, LoginActivity.class);
 			startActivity(i);
@@ -136,14 +112,6 @@ public class ProfileActivity extends BasicMenuActivity {
 		return true;
 	}
 	
-	/**
-	 * Allows users to edit their profile information.
-	 * Should start a new activity
-	 */
-	public void editProfile() {
-		// TODO: add button or link to layout
-	}
-	
 	public boolean isOnline() {
 	    ConnectivityManager cm =
 	        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -155,35 +123,12 @@ public class ProfileActivity extends BasicMenuActivity {
 	}
 	
 	/**
-	 * Gets the user's favorite recipes and adds each to the table layout favorites
-	 */
-	public void setFavorites() {
-		// TODO
-		dm = new DatabaseModel(this, DATABASE_NAME);
-		Cursor faves = dm.checkedItems(ThePantryContract.Recipe.TABLE_NAME, ThePantryContract.Recipe.FAVORITE);
-		
-		//LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		//View recipe = inflater.inflate(R.layout.list_result, favorites);
-		
-		if (faves != null) {
-			while (!faves.isAfterLast()) {
-				String recipe_name = faves.getString(0);
-				System.out.println(recipe_name);
-				SearchCriteria sc = new SearchCriteria("recipe", recipe_name);
-				//new ProfileSearchTask(this, "fave").execute(sc);
-				faves.moveToNext();
-			}
-			faves.close();
-		}
-		
-	}
-	
-	/**
 	 * Gets the user's cooking history and adds each to the table layout history
 	 */
 	public void setHistory() {
 		// TODO
 		dm = new DatabaseModel(this, DATABASE_NAME);
+		/*
 		Cursor cooked = dm.checkedItems(ThePantryContract.Recipe.TABLE_NAME, ThePantryContract.Recipe.COOKED);
 		
 		if (cooked != null) {
@@ -195,7 +140,13 @@ public class ProfileActivity extends BasicMenuActivity {
 				cooked.moveToNext();
 			}
 			cooked.close();
+		}*/
+		ArrayList<Storage> cookedRecipes = dm.getCookOrFav(ThePantryContract.SearchMatch.TABLE_NAME, ThePantryContract.SearchMatch.COOKED);
+		dm.close();
+		for (Storage recipe : cookedRecipes) {
+			cooked.add((SearchMatch)recipe);
 		}
+		System.out.println("Can I cast Recipe to SearchMatch object");
 	}
 	
 	
