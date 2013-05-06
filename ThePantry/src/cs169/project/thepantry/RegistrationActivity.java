dafe596de0fd94f0a01bf15dc8a1d8876c6efa5e
James Myers <jfmyers9@gmail.com>
2013-05-06 12:57:51 -0700

    small bug fixes in a few places

--- ThePantry/src/cs169/project/thepantry/RegistrationActivity.java
+++ ThePantry/src/cs169/project/thepantry/RegistrationActivity.java
@@ -16,7 +16,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,6 +23,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -64,6 +64,10 @@ public class RegistrationActivity extends Activity {
 		mPasswordView = (EditText) findViewById(R.id.password);
 		mPasswordConfView = (EditText) findViewById(R.id.password_confirmation);
 		mEmailView.setText(mEmail);
+		
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
 		
 		findViewById(R.id.registerButton).setOnClickListener(
 				new View.OnClickListener() {
@@ -128,28 +132,28 @@ public class RegistrationActivity extends Activity {
 
 		boolean cancel = false;
 		View focusView = null;
-//
-//		// Check for a valid password.
-//		if (TextUtils.isEmpty(mPassword)) {
-//			mPasswordView.setError(getString(R.string.error_field_required));
-//			focusView = mPasswordView;
-//			cancel = true;
-//		} else if (mPassword.length() < 8) {
-//			mPasswordView.setError(getString(R.string.error_invalid_password));
-//			focusView = mPasswordView;
-//			cancel = true;
-//		} else if (mPasswordConf.length() < 8) {
-//			mPasswordConfView.setError(getString(R.string.error_invalid_password));
-//			focusView = mPasswordView;
-//			cancel = true;
-//		}
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 8) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPasswordConf.length() < 8) {
+			mPasswordConfView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
 
 		// Check for a valid email address.
-//		if (TextUtils.isEmpty(mEmail)) {
-//			mEmailView.setError(getString(R.string.error_field_required));
-//			focusView = mEmailView;
-//			cancel = true;
-//		} 
+		if (TextUtils.isEmpty(mEmail)) {
+			mEmailView.setError(getString(R.string.error_field_required));
+			focusView = mEmailView;
+			cancel = true;
+		} 
 
 		if (cancel) {
 			// There was an error; don't attempt login and focus the first
@@ -158,6 +162,8 @@ public class RegistrationActivity extends Activity {
 		} else {
 			// Show a progress spinner, and kick off a background task to
 			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
 	    	String user = mEmail;
 	    	String password = mPassword;
 	    	String passConf = mPasswordConf;
@@ -221,6 +227,7 @@ private class GetJsonObject extends AsyncTask<String, String, JSONObject> {
 	            JSONObject respObj = new JSONObject(result);
 	            return respObj;
 	    	} catch (Exception e) {
+	    		showProgress(false);
 	    		e.printStackTrace();
 	    		return new JSONObject();
 	    	}
@@ -240,18 +247,22 @@ private class GetJsonObject extends AsyncTask<String, String, JSONObject> {
 						editor.putString(LOGGED_IN, auth_token);
 						editor.commit();
 					} else {
+						showProgress(false);
 						Context context = getApplicationContext();
 						CharSequence text = "Something went horribly wrong3.";
 						int duration = Toast.LENGTH_LONG;
 						Toast toast = Toast.makeText(context, text, duration);
 						toast.show();
 					}
+					showProgress(false);
 					Context context = getApplicationContext();
 					Intent intent = new Intent(context, TutorialActivity.class);
 					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 					intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 					startActivity(intent);
+					finish();
 				} else if (!success) {
+					showProgress(false);
 					Context context = getApplicationContext();
 					CharSequence text = info;
 					int duration = Toast.LENGTH_LONG;
@@ -259,6 +270,7 @@ private class GetJsonObject extends AsyncTask<String, String, JSONObject> {
 					toast.show();
 				}
 			} catch (Exception e) {
+				showProgress(false);
 				System.out.println(e.getMessage());
 				Context context = getApplicationContext();
 				CharSequence text = "Something went horribly wrong2.";
